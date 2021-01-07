3a40d11a57ec7789e0780ba1a902df1900aa210c
Eli Rykoff <erykoff@stanford.edu>
2021-01-07 11:45:45 -0800

    Copy in libtool configs

--- recipe/install_libapr.sh
+++ recipe/install_libapr.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
 
+set -x
+
 cd "${SRC_DIR}/apr"
+
+# Get an updated config.sub and config.guess
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* ./build
+
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
+  export ac_cv_file__dev_zero=yes
+#  export ac_cv_func_setpgrp_void=
+#  export apr_cv_process_shared_works=??
+#  export apr_cv_mutex_robust_shared=??
+#  export apr_cv_tcp_nodelay_with_cork=??
+fi
+
 ./configure --prefix="${PREFIX}" --host="${HOST}"
 make
 make install
