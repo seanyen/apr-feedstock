959c18126f9991d04f69ba7b045921e0bbcb1c64
Eli Rykoff <erykoff@stanford.edu>
2021-01-27 09:25:54 -0800

    Fix osx-arm64 cross-compile and SIGABRT memory errors.

--- recipe/install_libapr.sh
+++ recipe/install_libapr.sh
@@ -12,8 +12,12 @@ if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" && $target_platform == "osx-arm64"
   export ac_cv_func_setpgrp_void=yes
   export apr_cv_process_shared_works=yes
   export apr_cv_mutex_robust_shared=no
+  export apr_cv_mutex_recursive=yes
   export apr_cv_tcp_nodelay_with_cork=no
   export ac_cv_sizeof_struct_iovec=16
+  export ap_cv_atomic_builtins=yes
+  export apr_cv_gai_addrconfig=yes
+  export ac_cv_sizeof_pid_t=4
 fi
 
 autoreconf -vfi
